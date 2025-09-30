c947f6d561c3b1cd9dcb97a4326911104d3c8886
Divine Ezeilo <divineezeilo123@gmail.com>
2025-09-30 15:14:38 +0200

    Uppdated Decision tree

--- DecisionTree.py
+++ DecisionTree.py
@@ -9,7 +9,7 @@ from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import StandardScaler
 from sklearn.compose import ColumnTransformer
 from sklearn.base import BaseEstimator, TransformerMixin
-from sklearn.linear_model import Ridge
+from sklearn.tree import DecisionTreeRegressor
 from sklearn.metrics import mean_squared_error
 from sklearn.model_selection import GridSearchCV
 from sklearn.model_selection import cross_val_score
@@ -99,5 +99,70 @@ full_pipeline = ColumnTransformer([
 X_train_prepared = full_pipeline.fit_transform(X_train)
 X_test_prepared = full_pipeline.transform(X_test)
 
-print(X_test_prepared.shape)
-print(X_train_prepared.shape)
+
+
+
+# Grid Search
+param_grid = {
+    "max_depth": [None, 3, 5, 10, 15],
+    "min_samples_split": [2, 5, 10, 25],
+    "min_samples_leaf": [1, 2, 4, 6, 10],
+    "max_features": [None, "sqrt", "log2"]
+}
+
+tree_reg = DecisionTreeRegressor(random_state=42)
+grid_search = GridSearchCV(
+    tree_reg,
+    param_grid,
+    cv=5,
+    scoring='neg_mean_squared_error',
+    return_train_score=True
+)
+grid_search.fit(X_train_prepared, y_train)
+
+#Best hyperparameters
+print("Best parameter: ", grid_search.best_params_)
+print("Best CV RMSE: ", np.sqrt(-grid_search.best_score_))
+
+#Test RMSE
+best_Tree = grid_search.best_estimator_
+y_pred = best_Tree.predict(X_test_prepared)
+test_rmse = np.sqrt(mean_squared_error(y_test, y_pred))
+print("Test RMSE: ", test_rmse)
+
+#Best parameter:  {'max_depth': 10, 'max_features': None, 'min_samples_leaf': 10, 'min_samples_split': 25}
+#Best CV RMSE:  58724.839243191505
+#Test RMSE:  59890.34259107803
+
+
+# Randomized Search
+
+rnd_search = RandomizedSearchCV(
+    tree_reg,
+    param_distributions=param_grid,
+    n_iter=10,
+    cv=5,
+    scoring='neg_mean_squared_error',
+    random_state=42
+)
+
+rnd_search.fit(X_train_prepared, y_train)
+
+#Best CV RMSE & Parameters
+cvres = rnd_search.cv_results_
+for mean_score, params in zip(cvres["mean_test_score"], cvres["params"]):
+    print("CV RMSE: ", np.sqrt(-mean_score), "Params", params)
+
+print("Best Parameter: ", rnd_search.best_estimator_)
+print()
+# Test Prediction
+best_rnd_tree = rnd_search.best_estimator_
+y_rnd_pred = best_rnd_tree.predict(X_test_prepared)
+
+#Test RMSE
+test_rnd_rmse = np.sqrt(mean_squared_error(y_test, y_rnd_pred))
+print("Test RMSE: ", test_rnd_rmse)
+
+# Best Parameter:  DecisionTreeRegressor(max_depth=10, min_samples_leaf=10, random_state=42)
+# CV RMSE:  58923.61092011712 Params {'min_samples_split': 2, 'min_samples_leaf': 10, 'max_features': None, 'max_depth': 10}
+# Test RMSE:  59934.706056014074
